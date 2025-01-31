be905b7c25fe6e7a7ad1eaf9b49c2b517b4e10ad
gilmerperez <gilmer2002@outlook.com>
2025-01-30 23:45:56 -0500

    Changed removeCity to deleteCity in historyService.ts

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -30,7 +30,7 @@ class HistoryService {
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  async removeCity(id: string) {}
+  async deleteCity(id: string) {}
 }
 
 export default new HistoryService();
